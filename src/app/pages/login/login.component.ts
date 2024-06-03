0395f9044dc99f614f528b793b3323cf296336fc
Artur Bruno <arturjuliao20@gmail.com>
2024-06-03 10:16:38 -0300

    refactor: fix login page ui

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { AuthService } from './auth.service';
-import { Router } from '@angular/router';
+import {Component} from '@angular/core';
+import {AuthService} from './auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,21 +11,18 @@ export class LoginComponent {
   email: string = '';
   password: string = '';
   loginError: string = '';
-  
+
   constructor(private authService: AuthService, private router: Router) {}
 
   onLogin() {
     this.authService.login(this.email, this.password).subscribe(
       (response) => {
-      
         this.router.navigate(['/admin']);
       },
       (error) => {
-      
-        this.loginError = error.error.message;
-        console.log(this.loginError)
+        this.loginError = error.error.detail;
       }
     );
   }
-  
+
 }
