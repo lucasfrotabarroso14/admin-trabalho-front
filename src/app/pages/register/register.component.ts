86f69516f2d5dfc475d4365fda687da92d7f22fe
Artur Bruno <arturjuliao20@gmail.com>
2024-06-03 10:24:29 -0300

    refactor: fix register page ui

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { AuthService } from '../login/auth.service';
-import { Router } from '@angular/router';
+import {Component} from '@angular/core';
+import {AuthService} from '../login/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,32 +11,20 @@ export class RegisterComponent {
   email: string = '';
   name: string = '';
   password: string = '';
-  visible: boolean = false;
-  dialogMessage: string = '';
   registerError: string = '';
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  openAndCloseDialog() {
-    this.visible = !this.visible;
-  }
 
   onRegister() {
     this.authService.register(this.name, this.email, this.password).subscribe(
       (response) => {
-    
-        this.dialogMessage = 'Registro bem-sucedido! Por favor, faça login.';
-        this.openAndCloseDialog();
         setTimeout(() => {
-          this.openAndCloseDialog();
           this.router.navigate(['/login']);
         }, 3000);
       },
       (error) => {
-        
         this.registerError = error.error.message;
-        this.dialogMessage = 'Erro no registro: ' + this.registerError;
-        this.openAndCloseDialog();
       }
     );
   }
